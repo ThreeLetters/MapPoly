6063326115927b72d810f6434333562e3ffa1ddb
Andrew S <andrews54757@gmail.com>
2016-12-14 13:06:26 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-
+Map.prototype.every = function (c) {
+    var a = this.entries()
+    var b;
+    while (b = a.next().value) {
+        if (!c(b[1], b[0])) return true;
+    }
+  return false;
+}
+Map.prototype.toArray = function () {
+    var array = [];
+    this.forEach(function (a) {
+        array.push(a)
+    })
+    return array
+}
+Map.prototype.map = function(c) {
+  var f = new Map();
+ var a = this.entries()
+    var b;
+    while (b = a.next().value) {
+        if (c(b[1], b[0])) f.set(b[0],b[1])
+    }
+  return f;
+  
+}
